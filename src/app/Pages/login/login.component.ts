9c898e4ff17bc8959469637f96ad36eb1ca65795
Esteban Raffo <e.fraffo@gmail.com>
2022-08-28 17:29:01 -0300

    28/08

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  // Variable de Clase
+  title = "Login" 
+  modificado = false
+  myLogin:FormGroup
 
-  constructor() { }
+  constructor(private fl:FormBuilder) {
+    this.myLogin = this.fl.group({
+      email:["",[Validators.required]],
+      password:["",[Validators.required]]  
+    })
+  }
+  
+  login(){
+    console.log(this.myLogin.value)
+  }
 
+  cambiarTitle(){
+    this.title = "Login Angular"
+    this.modificado = true
+  }
+  
   ngOnInit(): void {
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,27 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-// import { SaludoComponent } from './saludo/saludo.component';
+import { LoginComponent } from './Pages/login/login.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { RegistroComponent } from './Pages/registro/registro.component';
-// import { BannersComponent } from './banners/banners.component';
 import { CatalogoComponent } from './catalogo/catalogo.component';
-import { DetalleComponent } from './detalle/detalle.component';
+import { DetalleComponent } from './Pages/detalle/detalle.component';
 import {HttpClientModule} from '@angular/common/http';
 import { MenuComponent } from './Components/menu/menu.component';
 import { ListadosModule } from './listados/listados.module';
-// import { ConcatenarPipe } from './concatenar.pipe'
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    // SaludoComponent,
     HomeComponent,
     RegistroComponent,
-    // BannersComponent,
     CatalogoComponent,
     DetalleComponent,
     MenuComponent
-    // ConcatenarPipe
   ],
   imports: [
     BrowserModule,
